8a8249ba517b89acd0464259abb9fe794f909f24
Vinicius Zanoli <viniciusdasilva.92hotmail.com>
2020-10-20 23:49:37 -0300

    Comentarios

--- src/test/java/tests/SignTest.java
+++ src/test/java/tests/SignTest.java
@@ -10,19 +10,32 @@ import static org.junit.Assert.*;
 public class SignTest {
     @Test
     public void testRealizaLogin(){
+        // SETA NAS PROPRIEDADES O CAMINHO DO WEB DRIVER QUE BAIXAMOS PARA REALIZAR A AUTOMAÇÃO
         System.setProperty("webdriver.chrome.driver", "/home/zanoli/Downloads/chromedriver/chromedriver");
         String login = "vinicius92", password = "123456";
 
+        // INTANCIANDO O CHROMEDRIVER
         WebDriver navegador = new ChromeDriver();
+
+        // SETA O VALOR DE TIMEOUT, POR CONTA DE QUANDO REALIZADO O LOGIN DEMORA UM POUCO PARA CARREGAR AS INFORMAÇÕES
         navegador.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 
+        // PEGA O SITE QUE SERA TESTADO NA AUTOMAÇÃO
         navegador.get("http://www.juliodelima.com.br/taskit");
 
+        // REALIZA CLICK NO LINK ( Sign in )
         navegador.findElement(By.linkText("Sign in")).click();
+
+        // ENVIA DADOS DO LOGIN
         navegador.findElement(By.id("signinbox")).findElement(By.name("login")).sendKeys(login);
+
+        // ENVIA DADOS DO PASSWORD
         navegador.findElement(By.id("signinbox")).findElement(By.name("password")).sendKeys(password);
+
+        // REALIZA CLICK NO ( SIGN IN" )
         navegador.findElement(By.linkText("SIGN IN")).click();
 
+        // BUSCA VALOR PELA NOME DA CLASSE
         String saldacao = navegador.findElement(By.className("me")).getText();
 
         assertEquals("Hi, Vinicius Zanoli", saldacao);
