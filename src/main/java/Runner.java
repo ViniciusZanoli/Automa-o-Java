5fa7ced9baa7e21bed6262ac6bb58fc6c34deaa5
viniciuszanoli <viniciusdasilva.92@hotmail.com>
2021-03-08 01:57:52 -0300

    Ajuste maximizar tela

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class Runner {
+class Teste {
+    int Teste = 305;
+
+    void Teste() {
+        System.out.println(Teste);
+    }
+
+    public static void main(String[] args) {
+        new Teste();
+    }
 }
